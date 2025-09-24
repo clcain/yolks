01dc55ffd6c574dc0ee4e6b69627ea99cdcd632c
Chandler Cain <chandler@clcain.com>
2025-09-24 00:58:10 -0400

    fix: use bin/bash -c

--- debian/entrypoint.sh
+++ debian/entrypoint.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 echo "Running default /entrypoint.sh"
+env
+echo
 
 if [ -f /mnt/install/install.sh ]
 then
     echo "Executing /mnt/install/install.sh"
     cat /mnt/install/install.sh
-    /mnt/install/install.sh
+    echo
+    /bin/bash /mnt/install/install.sh
 elif [ -f /mnt/startup/startup.sh ]
 then
     echo "Executing /mnt/startup/startup.sh"
     cat /mnt/startup/startup.sh
-    /mnt/startup/startup.sh
+    echo
+    /bin/bash /mnt/startup/startup.sh
 elif [ ! -z "$STARTUP" ]
 then
     echo "Executing \$STARTUP"
     echo "$STARTUP"
-    eval "$STARTUP"
+    echo
+    /bin/bash -c "$STARTUP"
 else
     echo "\$STARTUP is empty. Exiting."
 fi
